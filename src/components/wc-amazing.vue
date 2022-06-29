6015bbd6f2533693299c3cee0c3696ebd2352fbd
Luis Mori Guerra <luismorith@gmail.com>
2022-06-29 01:21:10 -0500

    feat: adding web component build
    Signed-off-by: Luis Mori Guerra <luismorith@gmail.com>

---
+++ src/components/wc-amazing.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    amazing web component
+    <pre class="pre">
+      your name is {{ name }}
+    </pre>
+    <hr />
+    <slot></slot>
+    <hr />
+    {{ adate }}
+    <hr />
+    <button>say my name</button>
+  </div>
+</template>
+
+<script>
+import { formatDistance, subDays } from "date-fns";
+export default {
+  name: "wc-displayname",
+  props: ["name"],
+  data() {
+    return {
+      adate: formatDistance(subDays(new Date(), 3), new Date(), {
+        addSuffix: true,
+      }),
+    };
+  },
+};
+</script>
+
+<style>
+:host .wrapper {
+  border: 5px solid blue;
+}
+button {
+  background: red;
+}
+</style>
